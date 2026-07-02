cb7728e106752260617e79769891ade9bc849474
ANMOL <sonianmol0918@gmail.com>
2026-07-03 01:22:59 +0530

    --

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Start FastAPI in the background
+uvicorn app.main:app --host 0.0.0.0 --port 8000 &
+
+# Start Streamlit
+streamlit run frontend/streamlit_app.py \
+  --server.port=$PORT \
+  --server.address=0.0.0.0
+  API_URL = "http://localhost:8000"
