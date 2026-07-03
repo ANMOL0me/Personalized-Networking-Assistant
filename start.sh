a2c3b263b52e3680b3c4935850a5ee3aa2ad4e70
ANMOL <sonianmol0918@gmail.com>
2026-07-03 21:17:26 +0530

    start.sh file changed

--- start.sh
+++ start.sh
@@ -3,8 +3,3 @@
 # Start FastAPI in the background
 uvicorn app.main:app --host 0.0.0.0 --port 8000 &
 
-# Start Streamlit
-streamlit run frontend/streamlit_app.py \
-  --server.port=$PORT \
-  --server.address=0.0.0.0
- export API_URL=http://localhost:8000
